aec262e7499dd050f7ade466be32b9cf86d75fa3
malcolm staso <mstaso1@gmail.com>
2020-04-18 17:53:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,3 +7,11 @@ def display_board
   puts "-----------"
   puts "   |   |   "
 end
+
+def display_board (x)
+  puts "#{x[0]}|#{x[1]}|#{x[2]}"
+  puts "-----------"
+  puts "#{x[3]}|#{x[4]}|#{x[5]}"
+  puts "-----------"
+  puts "#{x[6]}|#{x[7]}|#{x[8]}"
+end
